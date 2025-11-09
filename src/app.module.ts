2e00847f994614f1de575ee1d9c9d864f81786f0
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2025-11-09 04:42:08 +0200

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { OrdersModule } from './orders/orders.module';
 import { AuthModule } from './auth/auth.module';
+import { TypesModule } from './types/types.module';
 import { AiModule } from './ai/ai.module';
 
 @Module({
@@ -15,6 +16,7 @@ import { AiModule } from './ai/ai.module';
     UsersModule,
     ProductsModule,
     OrdersModule,
+    TypesModule,
     AiModule,
   ],
 })
