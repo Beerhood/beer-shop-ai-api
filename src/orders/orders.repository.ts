f0eda5bc162ee85b8cc7ed83f37ac2617d461cfc
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2025-11-15 01:17:37 +0200

    feat: update order repository in order to return populated

--- src/orders/orders.repository.ts
+++ src/orders/orders.repository.ts
@@ -1,10 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { BaseRepository } from '@common/base.repository';
 import { IOrder, OrdersModel } from '@common/models/';
+import { FilterQuery, SortOrder } from 'mongoose';
 
 @Injectable()
 export class OrdersRepository extends BaseRepository<IOrder> {
   constructor() {
     super(OrdersModel);
+  }
+
+  override find(
+    filter: FilterQuery<IOrder>,
+    sort?: Record<string, SortOrder>,
+    limit?: number,
+    skip?: number,
+    search?: FilterQuery<IOrder>,
+    projection?: NonNullable<unknown> | string | string[],
+    options?: NonNullable<unknown>,
+  ) {
+    return super.find(filter, sort, limit, skip, search, projection, options).populate('products');
+  }
+
+  override findOne(
+    conditions: FilterQuery<IOrder>,
+    projection?: object | string | string[],
+    options?: object,
+  ) {
+    return super.findOne(conditions, projection, options).populate('products');
+  }
+
+  override findById(id: string, projection?: object | string | string[], options?: object) {
+    return super.findById(id, projection, options).populate('products');
   }
 }
