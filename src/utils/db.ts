1fb9fa833cff3cec2d31acae81602e85e93cc984
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2025-11-15 15:44:03 +0200

    refactor: update configuration flow

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,15 +1,19 @@
+import { ConfigService } from '@nestjs/config';
 import mongoose from 'mongoose';
-import * as process from 'process';
+import { AppConfiguration } from 'src/config/configuration';
 
 mongoose.Promise = global.Promise;
-const srv = <string>process.env.DB_SRV;
+const config = new ConfigService<AppConfiguration>();
+
+const nodeEnv = <string>config.get('nodeEnv');
+const srv = <string>config.get('db_srv');
 
 export async function getDBConnection() {
   const options = {
     serverSelectionTimeoutMS: 1000,
   };
 
-  if (process.env.NODE_ENV !== 'production') {
+  if (nodeEnv !== 'production') {
     mongoose.set('debug', true);
   }
 
