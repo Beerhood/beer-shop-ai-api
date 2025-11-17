5090294ff130d83f0e04e4e86a4a05f4da2f417d
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2025-11-17 03:11:09 +0200

    fix: update orders & products find() field handling and add JSDoc documentation

--- src/products/products.repository.ts
+++ src/products/products.repository.ts
@@ -8,8 +8,20 @@ export class ProductsRepository extends BaseRepository<Product> {
   constructor() {
     super(ProductsModel);
   }
+
+  /**
+   * Finds all the documents populated by 'type' field that match conditions
+   * @param {object} filter
+   * @param {object} sort
+   * @param {number} limit
+   * @param {number} skip
+   * @param {object|String|Array<String>} projection
+   * @param {object} options
+   * @see {@link https://mongoosejs.com/docs/api/model.html#model_Model.find}
+   * @returns {*}
+   */
   override find(
-    filter: FilterQuery<Product>,
+    filter?: FilterQuery<Product>,
     sort?: Record<string, SortOrder>,
     limit?: number,
     skip?: number,
@@ -20,6 +32,14 @@ export class ProductsRepository extends BaseRepository<Product> {
     return super.find(filter, sort, limit, skip, search, projection, options).populate('type');
   }
 
+  /**
+   * Finds the first document populated by 'type' field that matches conditions
+   * @param {object} conditions
+   * @param {object|String|Array<String>} projection
+   * @param {object} options
+   * @see {@link https://mongoosejs.com/docs/api/model.html#model_Model.findOne}
+   * @returns {*}
+   */
   override findOne(
     conditions: FilterQuery<Product>,
     projection?: object | string | string[],
@@ -28,6 +48,14 @@ export class ProductsRepository extends BaseRepository<Product> {
     return super.findOne(conditions, projection, options).populate('type');
   }
 
+  /**
+   * Finds a single document populated by 'type' field by its _id field
+   * @param {string} id
+   * @param {object|String|Array<String>} projection
+   * @param {object} options
+   * @see {@link https://mongoosejs.com/docs/api/model.html#model_Model.findById}
+   * @returns {*}
+   */
   override findById(id: string, projection?: object | string | string[], options?: object) {
     return super.findById(id, projection, options).populate('type');
   }
