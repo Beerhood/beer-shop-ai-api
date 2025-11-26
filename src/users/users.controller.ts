5a50654e682eee7e0697f9e4921ce06659f563ac
bdendro <olegburia2072@gmail.com>
2025-11-26 09:56:37 +0200

    feat(user): add auth guards on user routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,22 +1,24 @@
-import { Body, Controller, Get, Inject, Patch } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Patch, Request, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { USER_ROUTE_PATH, USER_SERVICE_TOKEN } from './constants/user.const';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { NotEmptyBodyPipe } from '@common/pipes/not-empty.pipe';
+import { AuthGuard } from '@nestjs/passport';
+import { JwtPayloadRequest } from 'src/auth/interfaces/auth-requests.interface';
 
 @Controller(USER_ROUTE_PATH.CONTROLLER)
 export class UsersController {
   constructor(@Inject(USER_SERVICE_TOKEN) private readonly usersService: UsersService) {}
 
   @Get(USER_ROUTE_PATH.GET_PROFILE)
-  async findProfile() {
-    const id = process.env.TEST_USER_ID ?? ''; // TODO: remove after adding req.user
-    return await this.usersService.findProfile(id);
+  @UseGuards(AuthGuard('jwt'))
+  async findProfile(@Request() req: JwtPayloadRequest) {
+    return await this.usersService.findProfile(req.user.sub);
   }
 
   @Patch(USER_ROUTE_PATH.PATCH_PROFILE)
-  async update(@Body(NotEmptyBodyPipe) profile: UpdateUserDto) {
-    const id = process.env.TEST_USER_ID ?? ''; // TODO: remove after adding req.user
-    return this.usersService.updateProfile(id, profile);
+  @UseGuards(AuthGuard('jwt'))
+  async update(@Body(NotEmptyBodyPipe) profile: UpdateUserDto, @Request() req: JwtPayloadRequest) {
+    return await this.usersService.updateProfile(req.user.sub, profile);
   }
 }
